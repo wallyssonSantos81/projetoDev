80b5a29772f8b1d818440baacc419d606522e7f6
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-10-26 23:33:35 -0300

    Commit criações das classes

---
+++ claro-dev-week-2024/src/main/java/dio/me/claro_dev_week_2024/domain/model/Feature.java
@@ -0,0 +1,14 @@
+package dio.me.claro_dev_week_2024.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = " tb_feature")
+public class Feature extends BaseItem{
+
+}
+
+    
+
+    
+
+
---
+++ claro-dev-week-2024/src/main/java/dio/me/claro_dev_week_2024/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dio.me.claro_dev_week_2024.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import dio.me.claro_dev_week_2024.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
+
+
