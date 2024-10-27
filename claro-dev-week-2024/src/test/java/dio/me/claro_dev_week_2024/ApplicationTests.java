4a4b6b1421614674faafde717bfe6a2c3eb61bff
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-10-26 21:23:04 -0300

    Commit inicial

---
+++ claro-dev-week-2024/src/test/java/dio/me/claro_dev_week_2024/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.claro_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
